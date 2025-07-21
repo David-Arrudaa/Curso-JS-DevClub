3ccb280288ceb822a194786ec6f9bc0d7db1b73e
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-21 00:35:06 -0300

    Finalizando modulo1 de JS e iniciando modulo JSWeb

---
+++ moduloWeb/1-script.js
@@ -0,0 +1,21 @@
+console.log("Passei aqui");
+
+const mainInput = document.getElementById("main-input")
+console.log(mainInput);
+
+const elements = document.getElementsByClassName("paragraph-js")
+console.log(elements);
+
+
+/* 
+    document -> HTML
+    
+    getElementeById -> Trás UM elemento pelo ID
+    getElementByClassName -> Trás TODOS os elementos com essa classe
+    getElementByTagName -> Trás TODOS os elementos com essa TAG
+    getElementByName -> Trás TODOS os elementos com esse NAME
+    
+    querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
+    queySelectorAll -> Trás TODOS os elementos que encontrar
+
+    */
---
+++ moduloWeb/dom.js
@@ -0,0 +1,9 @@
+/* 
+DOM (DOCUMENT OBJECT MODEL) = Arvore de elementos do Navegador
+
+
+meu index.html seria a planta da casa, o navegador cria a casa a partir da planta,
+ o js vem e manipula essa casa, seja pintando a parede ou algo do tipo,
+ isso a partir de algum comando,
+ como se fosse o chefe da obra mandando o funcionario pintar a casa
+*/
