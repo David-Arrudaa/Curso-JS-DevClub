ae2bff1c0af22c481e5e5d949080992d213c183a
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-21 19:57:08 -0300

    Adicionando novas aulas de JsWeb

--- moduloWeb/3-script.js
+++ moduloWeb/3-script.js
@@ -2,3 +2,9 @@
     ALTERANDO CSS(ESTILOS) NO JAVASCRIPT
 
 */
+
+const button = document.querySelector(".main-button")
+
+button.style.backgroundColor = "#ffffff"
+button.style.color = "#ff0000"
+button.style.fontSize = "40px"
---
+++ moduloWeb/4-script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#main-input")
+
+function cliqueiNoBotao(){
+    console.log(input.value);
+    
+}
+
+function digiteinoInput(){
+    console.log("Digite no input");
+    
+}
+
