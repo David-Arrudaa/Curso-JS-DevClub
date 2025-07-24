6c739f9eee6ca2c5ca372d0524eaeab85e85fa6b
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-24 02:28:12 -0300

    Adiciona projeto random

---
+++ modulo2/math.js
@@ -0,0 +1,29 @@
+/* 
+Math -> Matematica
+
+- pow(2,2) / potencia / 2² = 4
+-sqrt(25,2) / raiz / 25
+- PI
+- ceil / teto -> arredondar para cima
+- floor / chão -> arredondar para baixo
+
+- random() / numero aleatorio de 0 e 1
+*/
+
+const result = Math.pow(2,2)
+console.log(result);
+
+const result2 = Math.sqrt(25,2)
+console.log(result2);
+
+const result3 = Math.PI
+console.log(result3);
+
+const result4 = Math.ceil(3.14)
+console.log(result4);
+
+const result5 = Math.floor(3.14)
+console.log(result5);
+
+const result6 = Math.random()
+console.log(result6);
