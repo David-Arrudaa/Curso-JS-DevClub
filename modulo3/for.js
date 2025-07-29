af16a44fd208cac388dbe916877b4e0ddedeaa27
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-28 22:09:44 -0300

    Inicia moulo3 de Js

---
+++ modulo3/for.js
@@ -0,0 +1,21 @@
+/* 
+- FOR
+    1) INICIALIZAÇÃO - Criar uma variavel e colocar um valor inicial para ela
+    2) CONDIÇÃO - Enquanto for true, p laço continuara iterando
+    3) EXPRESSAO FINAL - O que irá ocorrer a cada vez que nosso laço 
+
+    for([inicialização]; [condição]; [expressão final]){
+        Seu código aqui
+    }
+
+*/
+
+// for (let i = 0; i <= 15; i++) {
+//     console.log(i);
+// }
+
+const user = ["Maria", "Joao", "Juca", "Pedro"]
+
+for( let i = 0; i < user.length; i++){
+    console.log(user[i])
+}
