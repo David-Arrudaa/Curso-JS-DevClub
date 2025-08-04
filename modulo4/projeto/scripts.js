2e75c94f98fbb05c72eac8053f8551d3e3d52406
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-03 23:58:37 -0300

    Adionca funcionalidade botão for-each do projeto

--- modulo4/projeto/scripts.js
+++ modulo4/projeto/scripts.js
@@ -1,1 +1,24 @@
-console.log(menuOptions);
+const list = document.querySelector("ul")
+const buttonForEach = document.querySelector("#for-each")
+
+let myLi = ""
+
+function showAll() {
+    menuOptions.forEach(product => {
+        myLi += `
+            <li>
+                <img src=${product.src}>
+                <p>${product.name}</p>
+                 <p class="item-price">R$ ${product.price}</p>
+            </li>
+        `
+        list.innerHTML = myLi
+    })
+}
+
+buttonForEach.addEventListener("click", showAll)
+
+
+
+
+
