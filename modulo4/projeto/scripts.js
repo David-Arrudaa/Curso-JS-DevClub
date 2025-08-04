0faa4052575ee14206abfd6a95a004ca73f1d00e
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-04 17:59:17 -0300

    Adiciona map no projeto do modulo4 de Js

--- modulo4/projeto/scripts.js
+++ modulo4/projeto/scripts.js
@@ -1,10 +1,10 @@
 const list = document.querySelector("ul")
 const buttonForEach = document.querySelector("#for-each")
+const buttomMap = document.querySelector("#mapear")
 
-let myLi = ""
-
-function showAll() {
-    menuOptions.forEach(product => {
+function showAll(productsArray) {
+    let myLi = ""
+    productsArray.forEach(product => {
         myLi += `
             <li>
                 <img src=${product.src}>
@@ -12,11 +12,22 @@ function showAll() {
                  <p class="item-price">R$ ${product.price}</p>
             </li>
         `
-        list.innerHTML = myLi
     })
+    list.innerHTML = myLi
 }
 
-buttonForEach.addEventListener("click", showAll)
+function mapAllItems() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product, // Spread Operator
+            price: product.price * 0.9,  
+    }))
+
+    showAll(newPrices)
+
+}
+
+buttonForEach.addEventListener("click", () => showAll(menuOptions))
+buttomMap.addEventListener("click", mapAllItems)
 
 
 
