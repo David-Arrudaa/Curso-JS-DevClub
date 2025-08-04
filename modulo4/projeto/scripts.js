f851ac402a3d933f7ec0173b37a851c5a242a4b5
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-04 19:28:46 -0300

    Adiciona o reduce ao modulo4 de Js

--- modulo4/projeto/scripts.js
+++ modulo4/projeto/scripts.js
@@ -1,17 +1,18 @@
 const list = document.querySelector("ul")
 const buttonForEach = document.querySelector("#for-each")
 const buttomMap = document.querySelector("#mapear")
+const buttomReduce = document.querySelector("#somar")
 
 function showAll(productsArray) {
     let myLi = ""
     productsArray.forEach(product => {
         myLi += `
-            <li>
-                <img src=${product.src}>
-                <p>${product.name}</p>
-                 <p class="item-price">R$ ${product.price}</p>
-            </li>
-        `
+                    <li>
+                        <img src=${product.src}>
+                        <p>${product.name}</p>
+                         <p class="item-price">R$ ${product.price.toFixed(2)}</p>
+                    </li>
+                 `
     })
     list.innerHTML = myLi
 }
@@ -19,15 +20,26 @@ function showAll(productsArray) {
 function mapAllItems() {
     const newPrices = menuOptions.map((product) => ({
         ...product, // Spread Operator
-            price: product.price * 0.9,  
+        price: product.price * 0.9,
     }))
-
     showAll(newPrices)
+}
 
+function sumAllItens() {
+    const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+     list.innerHTML =
+        `
+            <li>
+                <p>O valor toal dos itens é: R$ ${totalValue}</p>
+            </li>
+    
+        `
+     
 }
 
 buttonForEach.addEventListener("click", () => showAll(menuOptions))
 buttomMap.addEventListener("click", mapAllItems)
+buttomReduce.addEventListener("click", sumAllItens)
 
 
 
